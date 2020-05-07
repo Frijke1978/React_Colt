d576d1dc97a2d7c70ee7ad9b8a01dfb5734504d1
frijke1978 <frijke1978@outlook.com>
2020-05-07 09:21:09 +0200

    Add unstyled slider

--- color/src/ColorBox.js
+++ color/src/ColorBox.js
@@ -23,7 +23,7 @@ class ColorBox extends Component {
                     />
                     <div className={`copy-msg ${copied && "show"}`}>
                         <h1>copied!</h1>
-                        <p>{background}</p>
+                        <p>{this.props.background}</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
