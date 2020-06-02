2a7c0a64575154497b510dd17079891c4f338aab
frijke1978 <frijke1978@outlook.com>
2020-06-02 13:36:20 +0200

    Finish MiniPalette delete

--- color/src/App.js
+++ color/src/App.js
@@ -14,11 +14,18 @@ class App extends Component {
     this.state = { palettes: savedPalettes || seedColors};
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+  deletePalette(id){
+    this.setState(
+      st => ({palettes: st.palettes.filter(palette => palette.id !== id)}),
+      this.syncLocalStorage
+    );
   }
   savePalette(newPalette) {
     this.setState({ palettes: [...this.state.palettes, newPalette] },
@@ -59,7 +66,10 @@ class App extends Component {
           exact
           path='/'
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList 
+              palettes={this.state.palettes} 
+              deletePalette={this.deletePalette}
+              {...routeProps} />
           )}
         />
         <Route
