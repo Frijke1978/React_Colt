bdba83c945cfd0bba512e2dc1ef47b4e25db9d6f
frijke1978 <frijke1978@outlook.com>
2020-06-16 11:54:33 +0200

    hooks project

---
+++ hooks/src/TodoApp.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import uuid from "uuid/dist/v4";
+
+function TodoApp() {
+    const initialTodos = [
+        { id: 1, task: "Clean Fishtank", completed: false },
+        { id: 2, task: "Wash Car", completed: true },
+        { id: 3, task: "Grow Beard", completed: false }
+    ];
+    const [todos, setTodos] = useState(initialTodos);
+    const addTodo = newTodoText => {
+        setTodos([...todos, { id: uuid(), task: newTodoText, completed: false}]);
+    };
+    const removeTodo = todoId => {
+        // filter out removed todo
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        // call setTodos with new todo array
+        setTodos(updatedTodos);
+    }
+    const toggleTodo = todoId => {
+        const updatedTodos = todos.map(todo => 
+            todo.id === todoId ? {...todo, completed: !todo.completed } : todo
+        );
+        setTodos(updatedTodos);
+    };
+    const editTodo = (todoId, newTask) => {
+        const updatedTodos = todos.map(todo => 
+            todo.id === todoId ? {...todo, task: newTask } : todo
+        );
+        setTodos(updatedTodos);
+    };
+    return (
+        <Paper 
+            style={{
+                padding: 0,
+                margin: 0,
+                height: "100vh",
+                backgroundColor: "#fafafa"
+            }}
+            elevation={0}
+        >
+            <AppBar color="primary" position="static" style={{ height: "64px" }}>
+                <Toolbar>
+                    <Typography color="inherit">TODOS WITH HOOKS</Typography>
+                </Toolbar>
+            </AppBar>
+            <Grid container justify= "center" style={{ mariginTop: "1rem" }}>
+                <Grid item xs={11} md={8} lg={4}>
+                    <TodoForm addTodo={addTodo} />
+                    <TodoList 
+                        todos={todos} 
+                        removeTodo={removeTodo} 
+                        toggleTodo={toggleTodo}
+                        editTodo={editTodo}
+                    />
+                </Grid>
+            </Grid>
+        </Paper>
+        );
+    }
+
+export default TodoApp;
