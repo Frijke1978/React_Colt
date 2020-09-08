7261ced2ffe46d01056c32f0d199223962f54b77
frijke1978 <frijke1978@outlook.com>
2020-09-08 09:35:37 +0200

    splitting into 2 contexts

--- todos-context-usereducer/src/context/todos.context.js
+++ todos-context-usereducer/src/context/todos.context.js
@@ -1,19 +1,19 @@
 import React, { createContext, useReducer } from "react";
-import todoReducer from "../reducer/todo.reducer";
-import useTodoState from "../hooks/useTodoState";
-import { unstable_createMuiStrictModeTheme } from "@material-ui/core";
+import todoReducer from "../reducer/todo.reducer.js";
 const defaultTodos = [
-    { id: 1, task: "Mow the lawn using goats", completed: false },
-    { id: 2, task: "Release lady bugs in the garden", completed: true }
+  { id: 1, task: "Mow the lawn using goats", completed: false },
+  { id: 2, task: "Release lady bugs into garden", completed: true }
 ];
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-    const [todos, dispatch] = useReducer(todoReducer, defaultTodos)
-    return (
-        <TodosContext.Provider value={{todos, dispatch}}>
-            {props.children}
-        </TodosContext.Provider>
-    );
-};
-
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  return (
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+        {props.children}
+      </DispatchContext.Provider>
+    </TodosContext.Provider>
+  );
+}
