8dd434230fbcaf4e68e204a1e12f830771e882ac
frijke1978 <frijke1978@outlook.com>
2020-09-29 10:56:22 +0200

    Add entrypoint for vendor js file, add bootstrap js

--- nextjs/wp_code/webpack.common.js
+++ nextjs/wp_code/webpack.common.js
@@ -2,35 +2,39 @@ const path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-        entry: "./src/index.js",
-        plugins: [new HtmlWebpackPlugin({
-            template: "./src/template.html"
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader", // 3. Inject styles into DOM 
-                    "css-loader", // 2. Turns css into commonjs
-                    "sass-loader" // 1. Turns sass into css
-                ]
-            },
-            { 
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.(svg|png|jpg|gif)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[hash].[ext]",
-                        outputPath: "imgs"
+        entry: {
+            main: "./src/index.js",
+            vendor: "./src/vendor.js"
+        },
+        plugins: 
+            [new HtmlWebpackPlugin({
+                template: "./src/template.html"
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        "style-loader", // 3. Inject styles into DOM 
+                        "css-loader", // 2. Turns css into commonjs
+                        "sass-loader" // 1. Turns sass into css
+                    ]
+                },
+                { 
+                    test: /\.html$/,
+                    use: ["html-loader"]
+                },
+                {
+                    test: /\.(svg|png|jpg|gif)$/,
+                    use: {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[hash].[ext]",
+                            outputPath: "imgs"
+                        }
                     }
                 }
-            }
-        ]
-    }
-};
+            ]
+        }
+    };
